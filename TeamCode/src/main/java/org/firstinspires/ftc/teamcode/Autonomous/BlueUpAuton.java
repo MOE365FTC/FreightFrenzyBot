d6d450f229feabebb049877e6bfcdc412d8ce161
Eblob123 <ethanh74324@gmail.com>
2021-10-23 10:30:55 -0400

    added+tested BlueUp path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueUpAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueUpAuton.java
@@ -3,44 +3,37 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.apache.commons.math3.stat.descriptive.moment.VectorialCovariance;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleTankDrive;
-
 @Autonomous
-public class BlueDownAuton extends LinearOpMode {
-
+public class BlueUpAuton extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-
-//        SampleTankDrive drive = new SampleTankDrive(hardwareMap);
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        Pose2d startPose = new Pose2d(-40.0, 60.0, Math.toRadians(-90.0));
+        Pose2d startPose = new Pose2d(5.0, 60.0, Math.toRadians(-90.0));
         drive.setPoseEstimate(startPose);
 
         Trajectory trajectory1 = drive.trajectoryBuilder(startPose.plus(new Pose2d(0.0, 0.0, Math.toRadians(110.0))))
-            .back(15.0)
-            .build();
+                .back(45.0)
+                .build();
 
 
         Trajectory trajectory2 = drive.trajectoryBuilder(trajectory1.end())
-            .splineTo(new Vector2d(-45.0, 27.0), Math.toRadians(0.0))
-            .forward(2)
-            .build();
+                .splineTo(new Vector2d(-45.0, 27.0), Math.toRadians(0.0))
+                .forward(10)
+                .build();
 
         Trajectory trajectory3 = drive.trajectoryBuilder(trajectory2.end())
-            .back(5)
-            .splineTo(new Vector2d(-30.0, 42.0), Math.toRadians(0.0))
-            .build();
+                .back(6)
+                .splineTo(new Vector2d(-30.0, 42.0), Math.toRadians(0.0))
+                .build();
 
         Trajectory trajectory4 = drive.trajectoryBuilder(trajectory3.end(), true)
-            .splineTo(new Vector2d(13.0, 41.0), Math.toRadians(0.0))
-            .build();
-            
+                .splineTo(new Vector2d(13.0, 41.0), Math.toRadians(0.0))
+                .build();
+
         waitForStart();
 
         drive.turn(Math.toRadians(110.0));
@@ -54,9 +47,5 @@ public class BlueDownAuton extends LinearOpMode {
         drive.followTrajectory(trajectory3);
 
         drive.followTrajectory(trajectory4);
-
     }
-
-
-
 }
